e317df01d7f802de367f5d6eeb400cb4c15a2c16
mahmood.khurram@gmail.com <mahmood.khurram@gmail.com>
2018-07-18 22:58:24 +0100

    structure application for config, application-service and discovery

--- application-service/src/main/java/org/khurramsyed/applicationservice/ApplicationServiceApplication.java
+++ application-service/src/main/java/org/khurramsyed/applicationservice/ApplicationServiceApplication.java
@@ -2,8 +2,10 @@ package org.khurramsyed.applicationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationServiceApplication {
 
 	public static void main(String[] args) {
